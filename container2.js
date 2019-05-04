c695cac72cfbebdfbda1e5f68a372073923ee009
LeeJunSeob <jsv1130@kaist.ac.kr>
2019-05-04 18:31:14 +0900

    Update container2.js

--- container2.js
+++ container2.js
@@ -27,11 +27,53 @@ var locationDict = {
 	"W8" : "Educational Support Building"
 };
 var coordinateDict = {
-	"N13-1" : "290,220,20",
-	"E11" : "323,320,20",
-	"E9" : "310,345,20",
-	"W8" : "190,320,20"
+	"N13-1" : ["290","220","20"],
+	"E11" : ["323","320","20"],
+	"E9" : ["310","345","20"],
+	"W8" : ["190","320","20"]
 };
+
+
+//pin on the map start
+function addpin(list){
+a="'http://naver.com'";
+  for (var i=0; i<list.length ; i++){
+    // $('<area shape="circle" id="N13-1" target="_blank"  coords="290,220,20" href="https://www.naver.com" />').appendTo(".campusmap");
+    $('<div><img id="' + list[i] +'" src="./image/redpin2.png"' +
+    'style="position: absolute; LEFT:' + coordinateDict[list[i]][0] + 'px; TOP:' + coordinateDict[list[i]][1] +'px;  WIDTH:30px; HEIGHT:50px"' +
+    'onclick="window.location.href=' + a +'"/></div>').appendTo(".map");
+  }
+}
+// var $map = $('<map name="mapmap">').appendTo('.campusmap');
+// $('<area id="1" shape="rect" coords="75,300,125,400" onclick="writeDiv(this.id)" />').appendTo($map);
+// $('<area id="2" shape="rect" coords="175,300,225,400" onclick="writeDiv(this.id)" />').appendTo($map);
+// $('<area id="3" shape="rect" coords="275,300,325,400" onclick="writeDiv(this.id)" />').appendTo($map);
+
+
+function collectlocation(list){
+  var a=[];
+  for (var i=0; i<list.length ; i++){
+    a.push(list[i][3]);
+  }
+  a.sort();
+  return removeoverlap(a);
+}
+
+function removeoverlap(list){
+	var a=[];
+  a.push(list[0]);
+	for (var i=0 ; i<list.length-1 ; i++){
+    	if (list[i] != list[i+1]){
+        	a.push(list[i+1]);
+        }
+    }
+  return a;
+}
+
+addpin(collectlocation(eventList));
+//pin on the map end
+
+
 function writeData(l){
 	//Just For Adding Events
 	var newdata = firebase.database().ref('/4idiots/').push();
@@ -65,4 +107,4 @@ function showDetail(event){
 
 $( document ).ready(function(){
 
-}
+});
