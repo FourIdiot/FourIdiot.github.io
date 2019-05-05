5220c606acea530a2972d60a23721fe638bc7b45
jhc0609 <jhc0609@kaist.ac.kr>
2019-05-05 20:24:53 +0900

    Update container2.js

--- container2.js
+++ container2.js
@@ -102,6 +102,20 @@ $("#today_btn").on('click', function(){
 		addpin(collectlocation(timeSelectedList));
 });
 
+// //accordian test
+// var acc = document.getElementsByClassName("accordion");
+
+// for (var i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
 
 
 // [Subject,[Month,Date],[start,end],locationN,explanation,link,numofinterests]
