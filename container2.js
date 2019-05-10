d2c57e2a33994890159f1fc0b510e1c76fc9372a
wnddjs <50008400+wnddjs@users.noreply.github.com>
2019-05-10 15:21:15 +0900

    Update container2.js

--- container2.js
+++ container2.js
@@ -464,6 +464,7 @@ function showDetail(event){
 	// 		}
 	// 	}
 }
+<<<<<<< HEAD
 /*
 function moving_pin(){
 	$(".pin").animate({marginTop: "30px"},1200,"",function(){
@@ -473,6 +474,8 @@ function moving_pin(){
 	});
 }*/
 
+=======
+>>>>>>> parent of 70cdecc... d
 
 $(document).on('click','.heart', function(){
 	if ("rgb(128, 128, 128)" == $(this).css("color")){
